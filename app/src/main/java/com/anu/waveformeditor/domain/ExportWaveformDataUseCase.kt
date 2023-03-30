5a2d8b421911a2ad34bbe887e22b0502daa211ba
Anu <karounwianuoluwapo@gmail.com>
2023-03-30 17:02:17 +0100

    extract string to constant

--- app/src/main/java/com/anu/waveformeditor/domain/ExportWaveformDataUseCase.kt
+++ app/src/main/java/com/anu/waveformeditor/domain/ExportWaveformDataUseCase.kt
@@ -4,9 +4,13 @@ import com.anu.waveformeditor.data.FileIORepository
 
 class ExportWaveformDataUseCase(private val fileIORepository: FileIORepository) {
     suspend operator fun invoke(waveformData: List<Pair<Float, Float>>): String {
-        if (waveformData.isEmpty()){
-            throw IllegalArgumentException("No selected region")
+        if (waveformData.isEmpty()) {
+            throw IllegalArgumentException(NO_SELECTION_MESSAGE)
         }
         return fileIORepository.writeWaveFormDataToUri(waveformData)
+    }
+
+    companion object {
+        const val NO_SELECTION_MESSAGE = "No selected region"
     }
 }
